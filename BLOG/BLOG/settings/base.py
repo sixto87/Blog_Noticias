962550ee05865fd3907ae2f8bc5c46c591ee58d2
Matias <matias002@gmail.com>
2021-02-19 17:54:13 -0300

    Primera migracion, superuser, cambio a django 3.0

--- BLOG/BLOG/settings/base.py
+++ BLOG/BLOG/settings/base.py
@@ -23,9 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '5n_1_8-=)wcc582pwjh_#1$w#b)$%c6r3&42whuoc)m_$5_!iw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
-ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -73,12 +71,7 @@ WSGI_APPLICATION = 'BLOG.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+
 
 
 # Password validation
--- BLOG/BLOG/settings/local.py
+++ BLOG/BLOG/settings/local.py
@@ -1,1 +1,17 @@
 from .base import *
+
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'sql_server.pyodbc',
+        'NAME':'blog',
+        'Trusted_Connection':'yes',
+        'HOST': 'localhost\wincc',
+        'OPTIONS':{
+        	'driver':'SQL Server Native Client 11.0',
+        }
+    },
+}
